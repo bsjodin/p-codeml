d1aedacd1e6e59a3cb8a54eaef4beb0460b5a239
bsjodin <42284156+bsjodin@users.noreply.github.com>
2022-03-11 14:49:33 -0800

    Add files via upload

--- src/generate_ctl.sh
+++ src/generate_ctl.sh
@@ -2,6 +2,7 @@
 
 input=$1
 output=$2
+tree=$3
 
 mkdir -p $output
 
@@ -9,13 +10,13 @@ while read file;do
 mkdir -p $output/$file
 mkdir -p $output/$file/ModelA $output/$file/ModelAnull
 
-echo "seqfile = ${input}/${file}.pml" >> $output/$file/ModelA/codemlModelA.ctl
-echo "treefile = ${PWD}/tree_labeled.txt" >> $output/$file/ModelA/codemlModelA.ctl
+echo "seqfile = ${input}/${file}.pml" > $output/$file/ModelA/codemlModelA.ctl
+echo "treefile = ${tree}" >> $output/$file/ModelA/codemlModelA.ctl
 echo "outfile = A_mlc" >> $output/$file/ModelA/codemlModelA.ctl
 less $PWD/src/codemlModelA.ctl >> $output/$file/ModelA/codemlModelA.ctl
 
 echo "seqfile = ${input}/${file}.pml" >> $output/$file/ModelAnull/codemlModelAnull.ctl
-echo "treefile = ${PWD}/tree_labeled.txt" >> $output/$file/ModelAnull/codemlModelAnull.ctl
+echo "treefile = ${tree}" >> $output/$file/ModelAnull/codemlModelAnull.ctl
 echo "outfile = Anull_mlc" >> $output/$file/ModelAnull/codemlModelAnull.ctl
 less $PWD/src/codemlModelAnull.ctl >> $output/$file/ModelAnull/codemlModelAnull.ctl
 
